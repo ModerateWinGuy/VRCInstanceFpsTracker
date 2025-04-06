e3c60d565df6d4f2ec9487b22603ed4d90fd100f
Daniel <moderatewinguy@gmail.com>
2025-04-07 11:59:27 +1200

    update build configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,13 @@
 let userConfig = undefined
+
 try {
-  // try to import ESM first
-  userConfig = await import('./v0-user-next.config.mjs')
-} catch (e) {
-  try {
-    // fallback to CJS import
-    userConfig = await import("./v0-user-next.config");
-  } catch (innerError) {
-    // ignore error
-  }
+  // fallback to CJS import
+  userConfig = await import("./next.config.js");
+} catch (innerError) {
+  // ignore error
 }
 
+ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
